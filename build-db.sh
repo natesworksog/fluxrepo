137f6d8989f7097bfbb5afbb1a2da9a163bc182e
Natesworks <contact@natesworks.com>
2023-10-07 15:47:59 +0200

    Fix?

--- build-db.sh
+++ build-db.sh
@@ -1,16 +1,46 @@
+#!/usr/bin/env bash
+#
+# Original from dtos-core-repo
+# Script name: build-db.sh
+# Description: Script for rebuilding the database for fluxrepo.
+# GitLab: https://www.gitlab.com/dwt1/fluxrepo
+# Contributors: Derek Taylor
+
+# Set with the flags "-e", "-u","-o pipefail" cause the script to fail
+# if certain things happen, which is a good thing.  Otherwise, we can
+# get hidden bugs that are hard to discover.
 set -euo pipefail
 
+echo "###########################"
+echo "Building the repo database."
+echo "###########################"
+
+## Arch: x86_64
 cd x86_64
-rm -f flux-repo*
+rm -f fluxrepo*
 
-repo-add -s -n -R flux-repo.db.tar.gz *.pkg.tar.zst
+echo "###################################"
+echo "Building for architecture 'x86_64'."
+echo "###################################"
 
-rm flux-repo.db
-rm flux-repo.db.sig
-rm flux-repo.files
-rm flux-repo.files.sig
+## repo-add
+## -s: signs the packages
+## -n: only add new packages not already in database
+## -R: remove old package files when updating their entry
+repo-add -s -n -R fluxrepo.db.tar.gz *.pkg.tar.zst
 
-mv flux-repo.db.tar.gz flux-repo.db
-mv flux-repo.db.tar.gz.sig flux-repo-db.sig
-mv flux-repo.files.tar.gz flux-repo.files
-mv flux-repo.files.tar.gz.sig flux-repo.files.sig
+# Removing the symlinks because GitLab can't handle them.
+rm fluxrepo.db
+rm fluxrepo.db.sig
+rm fluxrepo.files
+rm fluxrepo.files.sig
+
+# Renaming the tar.gz files without the extension.
+mv fluxrepo.db.tar.gz fluxrepo.db
+mv fluxrepo.db.tar.gz.sig fluxrepo-db.sig
+mv fluxrepo.files.tar.gz fluxrepo.files
+mv fluxrepo.files.tar.gz.sig fluxrepo.files.sig
+
+echo "#######################################"
+echo "Packages in the repo have been updated!"
+echo "#######################################"
